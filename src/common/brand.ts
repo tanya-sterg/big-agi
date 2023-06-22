96464670227490db32f4367216763cbe106543fc
rodbastos <rodrigo@targetteal.com>
2023-06-22 15:50:00 -0300

    Update brand.ts

--- src/common/brand.ts
+++ src/common/brand.ts
@@ -13,10 +13,10 @@ export const Brand = {
     Common: (process.env.NODE_ENV === 'development' ? '[DEV] ' : '') + 'Target Teal',
   },
   Meta: {
-    SiteName: 'Target Teal',
+    SiteName: 'AI - Target Teal',
     Title: 'Assistentes Inteligentes para Org Design',
     Description: 'Assistentes de IA para apoiar o seu trabalho com Design Organizacional. Desenvolvido pela Target Teal',
-    Keywords: 'artificial general intelligence, org design, agi, openai, gpt-4, ai personas, code execution, pdf import, voice i/o, ai chat, artificial intelligence',
+    Keywords: 'artificial general intelligence, org design, agi, inteligência artificial, openai, gpt-4, ai personas, code execution, pdf import, voice i/o, ai chat, artificial intelligence',
     ThemeColor: '#434356',
     TwitterSite: '@targetteal',
   },
