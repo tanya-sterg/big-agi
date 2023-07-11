c9305a2b18ae9c9cb6551f0a7f3129d2f42aa6ec
rodbastos <rodrigo@targetteal.com>
2023-07-11 16:15:12 -0300

    colocando a chave da Openai hardcoded

--- src/modules/aifn/embeddings/embeddings.client.ts
+++ src/modules/aifn/embeddings/embeddings.client.ts
@@ -18,6 +18,7 @@ export async function callPublish(question: string): Promise<string | null> {
             dbHost: '59ae7db1-15f6-46e9-82c5-84b2ef7f2953',
             indexdb: 'tt-kb',
             docsCount:'1',
+            openaiKey: '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',
             origin: getOrigin(),
         };
 
