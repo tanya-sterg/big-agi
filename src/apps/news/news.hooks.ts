3706ed308fecc944534a6ed24c1985aefa06298e
rodbastos <rodrigo@targetteal.com>
2023-09-25 14:07:23 -0300

    Update news.hooks.ts
    teste

--- src/apps/news/news.hooks.ts
+++ src/apps/news/news.hooks.ts
@@ -1,22 +1,32 @@
-import * as React from 'react';
-import { shallow } from 'zustand/shallow';
+import { useState } from 'react';
 import { useRouter } from 'next/router';
-
 import { useAppStateStore } from '~/common/state/store-appstate';
 
-import { incrementalVersion } from './news.data';
-
-
-export function useShowNewsOnUpdate() {
+function KeywordForm() {
+  const [keyword, setKeyword] = useState('');
+  const [error, setError] = useState('');
   const { push } = useRouter();
-  React.useEffect(() => {
-    // Sempre redireciona para a página de notícias
-    push('/news').then(() => null);
-  }, [push]); // Dependência do useEffect
+  const setAccessGranted = useAppStateStore(state => state.setAccessGranted);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+    if (keyword === 'tt-ai-thebest') {
+      setAccessGranted(true); // Ou você poderia usar localStorage.setItem('accessGranted', true);
+      push('/another-page').then(() => null);
+    } else {
+      setError('Palavra-chave incorreta. Tente novamente.');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="keyword">Digite a palavra-chave:</label>
+      <input type="text" id="keyword" value={keyword} onChange={(e) => setKeyword(e.target.value)} />
+      <button type="submit">Enviar</button>
+      {error && <p>{error}</p>}
+    </form>
+  );
 }
 
-export function useMarkNewsAsSeen() {
-  React.useEffect(() => {
-    useAppStateStore.getState().setLastSeenNewsVersion(incrementalVersion);
-  }, []);
-}
+export default KeywordForm;
