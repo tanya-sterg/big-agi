10309533f2bdf57e0869aa7b258aa4e9702ee3c8
rodbastos <rodrigo@targetteal.com>
2023-09-25 14:10:26 -0300

    Update news.hooks.ts
    teste 2

--- src/apps/news/news.hooks.ts
+++ src/apps/news/news.hooks.ts
@@ -1,18 +1,18 @@
 import { useState } from 'react';
 import { useRouter } from 'next/router';
-import { useAppStateStore } from '~/common/state/store-appstate';
+import { useAppStateStore } from '~/common/state/store-appstate'; // Certifique-se de que o caminho do import está correto
 
 function KeywordForm() {
   const [keyword, setKeyword] = useState('');
   const [error, setError] = useState('');
   const { push } = useRouter();
-  const setAccessGranted = useAppStateStore(state => state.setAccessGranted);
+  const setAccessGranted = useAppStateStore(state => state.setAccessGranted); // Certifique-se de que setAccessGranted está definido corretamente no seu store
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    
+
     if (keyword === 'tt-ai-thebest') {
-      setAccessGranted(true); // Ou você poderia usar localStorage.setItem('accessGranted', true);
+      setAccessGranted(true);
       push('/another-page').then(() => null);
     } else {
       setError('Palavra-chave incorreta. Tente novamente.');
@@ -22,7 +22,12 @@ function KeywordForm() {
   return (
     <form onSubmit={handleSubmit}>
       <label htmlFor="keyword">Digite a palavra-chave:</label>
-      <input type="text" id="keyword" value={keyword} onChange={(e) => setKeyword(e.target.value)} />
+      <input
+        type="text"
+        id="keyword"
+        value={keyword}
+        onChange={(e) => setKeyword(e.target.value)}
+      />
       <button type="submit">Enviar</button>
       {error && <p>{error}</p>}
     </form>
