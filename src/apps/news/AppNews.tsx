417aa4543466ec38a4dd6de0f799006760266345
rodbastos <rodrigo@targetteal.com>
2023-09-25 18:19:15 -0300

    Update AppNews.tsx
    form na news

--- src/apps/news/AppNews.tsx
+++ src/apps/news/AppNews.tsx
@@ -1,27 +1,29 @@
 import * as React from 'react';
-
-import { Button, Card, CardContent, Container, IconButton, Sheet, Typography } from '@mui/joy';
+import { useRouter } from 'next/router';
+import { Button, Card, CardContent, Container, Sheet, Typography, IconButton } from '@mui/joy';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
-import { Brand } from '~/common/brand';
 import { Link } from '~/common/components/Link';
-import { capitalizeFirstLetter } from '~/common/util/textUtils';
-
 import { NewsItems } from './news.data';
-
+import { useMarkNewsAsSeen } from './news.hooks'; // Assuming the modified hook is in the same directory
 
 export default function AppNews() {
   // state
   const [lastNewsIdx, setLastNewsIdx] = React.useState<number>(0);
+  const [keyword, setKeyword] = React.useState<string>('');
+  const markNewsAsSeen = useMarkNewsAsSeen(); // Using the modified hook
 
   // news selection
   const news = NewsItems.filter((_, idx) => idx <= lastNewsIdx);
   const firstNews = news[0] ?? null;
 
-  return (
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    markNewsAsSeen(keyword); // Using the modified hook
+  };
 
+  return (
     <Sheet variant='soft' invertedColors sx={{
-      // background: theme.vars.palette.background.level2,
       display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
       flexGrow: 1,
       overflowY: 'auto',
@@ -29,7 +31,7 @@ export default function AppNews() {
       p: { xs: 3, md: 6 },
       gap: 4,
     }}>
-
+      
       <Typography level='display2'>
         Updates - Novidades!
       </Typography>
@@ -40,56 +42,62 @@ export default function AppNews() {
       </Typography>
 
       {!!news && <Container disableGutters maxWidth='sm'>
-        {news?.map((item, idx) => {
+        {news.map((item, idx) => {
           const firstCard = idx === 0;
           const hasCardAfter = news.length < NewsItems.length;
           const showExpander = hasCardAfter && (idx === news.length - 1);
           const addPadding = !firstCard || showExpander;
-          return <Card key={'news-' + idx} sx={{ mb: 2, minHeight: 32 }}>
-            <CardContent sx={{ position: 'relative', pr: addPadding ? 4 : 0 }}>
-              {!!item.text && <Typography component='div' level='body1'>
-                {item.text}
-              </Typography>}
+          return (
+            <Card key={'news-' + idx} sx={{ mb: 2, minHeight: 32 }}>
+              <CardContent sx={{ position: 'relative', pr: addPadding ? 4 : 0 }}>
+                {!!item.text && <Typography component='div' level='body1'>
+                  {item.text}
+                </Typography>}
 
-              {!!item.items && (item.items.length > 0) && <ul style={{ marginTop: 8, marginBottom: 8, paddingInlineStart: 32 }}>
-                {item.items.map((item, idx) => <li key={idx}>
-                  <Typography component='div' level='body1'>
-                    {item.text}
+                {!!item.items && (item.items.length > 0) && <ul style={{ marginTop: 8, marginBottom: 8, paddingInlineStart: 32 }}>
+                  {item.items.map((item, idx) => <li key={idx}>
+                    <Typography component='div' level='body1'>
+                      {item.text}
+                    </Typography>
+                  </li>)}
+                </ul>}
+
+                {!firstCard && (
+                  <Typography level='body2' sx={{ position: 'absolute', right: 0, top: 0 }}>
+                    {item.versionName}
                   </Typography>
-                </li>)}
-              </ul>}
+                )}
 
-              {!firstCard && (
-                <Typography level='body2' sx={{ position: 'absolute', right: 0, top: 0 }}>
-                  {item.versionName}
-                </Typography>
-              )}
-
-              {showExpander && (
-                <IconButton
-                  variant='plain' size='sm'
-                  onClick={() => setLastNewsIdx(idx + 1)}
-                  sx={{ position: 'absolute', right: 0, bottom: 0, mr: -1, mb: -1 }}
-                >
-                  <ExpandMoreIcon />
-                </IconButton>
-              )}
-
-            </CardContent>
-          </Card>;
+                {showExpander && (
+                  <IconButton
+                    variant='plain' size='sm'
+                    onClick={() => setLastNewsIdx(idx + 1)}
+                    sx={{ position: 'absolute', right: 0, bottom: 0, mr: -1, mb: -1 }}
+                  >
+                    <ExpandMoreIcon />
+                  </IconButton>
+                )}
+              </CardContent>
+            </Card>
+          );
         })}
       </Container>}
+      
+      <form onSubmit={handleSubmit}>
+        <label>
+          Enter Keyword to Proceed:
+          <input 
+            type="text" 
+            value={keyword} 
+            onChange={(e) => setKeyword(e.target.value)} 
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
 
       <Button variant='solid' color='neutral' size='lg' component={Link} href='/' noLinkStyle>
         Ok!
       </Button>
-
-      {/*<Typography level='body1' sx={{ textAlign: 'center' }}>*/}
-      {/*  Enjoy!*/}
-      {/*  <br /><br />*/}
-      {/*  -- The {Brand.Title.Base} Team*/}
-      {/*</Typography>*/}
-
     </Sheet>
   );
 }
