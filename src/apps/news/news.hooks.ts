b0dfd1270430a34a9e9136e9c4d9dd680c819f68
rodbastos <rodrigo@targetteal.com>
2023-09-25 18:26:48 -0300

    Update news.hooks.ts
    form na news 5

--- src/apps/news/news.hooks.ts
+++ src/apps/news/news.hooks.ts
@@ -1,16 +1,36 @@
-import { useCallback } from 'react'; // Directly importing useCallback
-import { useRouter } from 'next/router'; // Importing useRouter
+import * as React from 'react';
+import { useRouter } from 'next/router';
+import { shallow } from 'zustand/shallow';
+import { useAppStateStore } from '~/common/state/store-appstate';
+import { incrementalVersion } from './news.data';
 
 export function useShowNewsOnUpdate() {
-    // function implementation
+  const { push } = useRouter();
+  const { usageCount, lastSeenNewsVersion } = useAppStateStore(state => ({
+    usageCount: state.usageCount,
+    lastSeenNewsVersion: state.lastSeenNewsVersion,
+  }), shallow);
+
+  React.useEffect(() => {
+    const isNewsOutdated = (lastSeenNewsVersion || 0) < incrementalVersion;
+    if (isNewsOutdated && usageCount > 2) {
+      // Disable for now
+      push('/news').then(() => null);
+    }
+  }, [lastSeenNewsVersion, push, usageCount]);
 }
+
 export function useMarkNewsAsSeen() {
-  const { push } = useRouter(); // using useRouter
-  
-  return useCallback((keyword: string) => { // Directly using useCallback without React prefix
-    if(keyword === 'aitt23') {
+  const { push } = useRouter(); // Use useRouter to perform redirections
+
+  return React.useCallback((keyword: string) => {
+    if (keyword === 'aitt23') {
+      // If the keyword is correct, mark the news as seen and redirect to the app/home page
       useAppStateStore.getState().setLastSeenNewsVersion(incrementalVersion);
       push('/'); // redirects to the home or app page
+    } else {
+      // If the keyword is incorrect, show an alert
+      alert('Invalid Keyword!');
     }
   }, [push]);
 }
