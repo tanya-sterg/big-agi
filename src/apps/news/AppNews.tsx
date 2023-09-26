0cbbe4c748b546ea8aed19b9560b662930e9aa8a
Renato Chencinski <renato.chencinski@inspira.com.br>
2023-09-26 00:53:49 -0300

    Ajusta visual do news

--- src/apps/news/AppNews.tsx
+++ src/apps/news/AppNews.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
 import { Sheet, Typography, Container, Card, CardContent, IconButton } from '@mui/joy';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { NewsItems } from './news.data';
 import { useMarkNewsAsSeen } from './news.hooks';
 
 export default function AppNews() {
@@ -11,7 +9,6 @@ export default function AppNews() {
   const markNewsAsSeen = useMarkNewsAsSeen();
 
   // news selection
-  const news = NewsItems.filter((_, idx) => idx <= lastNewsIdx);
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
@@ -37,51 +34,9 @@ const greyBlue = "#5a5a72";
         Quando o limite de texto em um chat chegar ao máximo as mensagens mais antigas são apagadas, e você pode continuar conversando!
       </Typography>
 
-      {!!news && <Container disableGutters maxWidth='sm'>
-        {news.map((item, idx) => {
-          const firstCard = idx === 0;
-          const hasCardAfter = news.length < NewsItems.length;
-          const showExpander = hasCardAfter && (idx === news.length - 1);
-          const addPadding = !firstCard || showExpander;
-          return (
-            <Card key={'news-' + idx} sx={{ mb: 2, minHeight: 32 }}>
-              <CardContent sx={{ position: 'relative', pr: addPadding ? 4 : 0 }}>
-                {!!item.text && <Typography component='div' level='body1'>
-                  {item.text}
-                </Typography>}
-
-                {!!item.items && (item.items.length > 0) && <ul style={{ marginTop: 8, marginBottom: 8, paddingInlineStart: 32 }}>
-                  {item.items.map((item, idx) => <li key={idx}>
-                    <Typography component='div' level='body1'>
-                      {item.text}
-                    </Typography>
-                  </li>)}
-                </ul>}
-
-                {!firstCard && (
-                  <Typography level='body2' sx={{ position: 'absolute', right: 0, top: 0 }}>
-                    {item.versionName}
-                  </Typography>
-                )}
-
-                {showExpander && (
-                  <IconButton
-                    variant='plain' size='sm'
-                    onClick={() => setLastNewsIdx(idx + 1)}
-                    sx={{ position: 'absolute', right: 0, bottom: 0, mr: -1, mb: -1 }}
-                  >
-                    <ExpandMoreIcon />
-                  </IconButton>
-                )}
-              </CardContent>
-            </Card>
-          );
-        })}
-      </Container>}
-
       <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '8px' }}>
         <label style={{ color: greyBlue }}>
-          Entre com a palavra chave :
+          Entre com a palavra chave* :
         </label>
         <input
           type="text"
@@ -93,6 +48,8 @@ const greyBlue = "#5a5a72";
           Enviar
         </button>
       </form>
+
+      <div>*Visite <b><a href="https://targetteal.com/pt/ia/" target='_blank'>targetteal.com</a></b> para obter a palavra chave</div>
 
     </Sheet>
   );
